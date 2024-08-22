f16b99ce8284490a4fc41feb5d3e9d9b96e1a25a
Jose Contreras <mexidios.mx@gmail.com>
2024-08-21 22:44:12 -0600

    chore: agrega bootstrap-vue-next

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import * as BootstrapVueNext from 'bootstrap-vue-next'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -17,6 +19,11 @@ const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 
+for (const componentKey in BootstrapVueNext) {
+    app.component(componentKey, BootstrapVueNext[componentKey])
+}
+
 app.use(router)
+app.use(BootstrapVueNext)
 
 app.mount('#app')
