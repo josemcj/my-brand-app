c1eed66473325a8db6b8de464904d5163c871eed
Jose Contreras <mexidios.mx@gmail.com>
2024-08-21 22:11:50 -0600

    feat: limpia la alerta en n tiempo

---
+++ src/components/Alert.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const props = defineProps({
+    alertClass: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    dismiss: Number,
+})
+
+const show = ref(true)
+
+onMounted(() => {
+    if (props.dismiss) {
+        setTimeout(() => {
+            show.value = false
+        }, props.dismiss)
+    }
+})
+</script>
+
+<template>
+    <div v-if="show" :class="['alert', alertClass, 'text-center']" role="alert">
+        {{ message }}
+    </div>
+</template>
